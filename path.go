a9f923e17f2262f27145ecb7a29984d284f8f700
ik5 <idok@linesip.com>
2013-11-29 22:31:56 +0200

    the right way to use path ...

--- path.go
+++ path.go
@@ -4,31 +4,18 @@ import (
   "fmt"
   "os"
   "path"
-  "runtime"
 )
 
 func AppPath() (string, error) {
-  separator := func() string {
-    switch runtime.GOOS {
-      case "windows" :
-        return "\\"
-      case "linux" :
-        return "/"
-
-      default :
-        return "/"
-    }
-
-  }
   if path.IsAbs(os.Args[0]) {
-    return path.Dir(os.Args[0]) + separator(), nil
+    return path.Dir(os.Args[0]) + string(os.PathSeparator), nil
   }
   wd, err := os.Getwd()
   if err != nil {
     return "", err
   }
   fname := path.Clean(path.Join(wd, os.Args[0]))
-  return path.Dir(fname) + separator(), nil
+  return path.Dir(fname) + string(os.PathSeparator), nil
 
 }
 
